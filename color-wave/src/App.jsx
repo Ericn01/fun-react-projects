dbbca2a31b82b044281af97d0719d91b5311651e
Ericn01 <nielseneric2001@gmail.com>
2025-07-04 16:40:39 -0600

    Added a favicon to color-wave and resolved some merge issues from before.

--- color-wave/src/App.jsx
+++ color-wave/src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import { ColorForm } from './components/ColorForm.jsx';
 import { ColorCanvas } from './components/ColorCanvas.jsx';
 
