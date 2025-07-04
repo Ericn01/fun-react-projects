fa0637d284efd15f78e09f29e6e0f964ccd89c2a
Ericn01 <nielseneric2001@gmail.com>
2025-07-04 17:09:58 -0600

    Added some CSS magic to make the project landing page look presentable.

--- spacex-launch-tracker/vite.config.js
+++ spacex-launch-tracker/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fun-react-projects/space-x-launch-tracker/',
+  base: '/fun-react-projects/spacex-launch-tracker/',
   plugins: [react(), tailwindcss()],
 })
